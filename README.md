a690c86e4853d4a022c50dd5548dddef3415079c
tvidano <61514829+tvidano@users.noreply.github.com>
2024-02-01 17:14:45 -0800

    Improve instructions in readme and formatting of code.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ros_vehicle_sim
-A simple package of vehicle dynamics models with ROS interfaces.
+A simple package of vehicle dynamics models with ROS interfaces. The odometry that is published by the vehicle model is under the `/novatel/oem7/odom` topic name and uses the `nav_msgs/Odometry.h` message type. The vehicle model accepts velocity inputs through the `vehicle/accelerator_pedal_cmd` topic, which uses the header:`raptor_dbw_msgs/AcceleratorPedalCmd.h`. The vehicle model accepts steering wheel inputs through the `vehicle/steering_cmd` topic, which uses the header:`raptor_dbw_msgs/SteeringCmd.h`. This is so that the simulator can replace an actual vehicle equipped with a raptor dbw system and a novatel INS system.
+
+## Single Track Model (Bicycle Car Model)
+The single track model currently implemented is parameterized for a Jeep Grand Cherokee. Currently, this is implemented in the `DynamicBike` class in `bike.h`.
+
+# Building
+To build the ros package navigate to the `ros_vehicle_sim` directory and run `catkin_make`.
+
+# Running
+To run the ros package:
+1. Source the setup file: `source devel/setup.bash`
+2. In a separate terminal, start a ros master: `roscore`
+2. Run the simulator: `rosrun ros_vehicle_sim ros_vehicle_sim_node`
--- src/ros_vehicle_sim/src/ros_vehicle.cpp
+++ src/ros_vehicle_sim/src/ros_vehicle.cpp
@@ -14,155 +14,155 @@
 #include "bike.h"
 
 class DynamicBikeNode {
-    private:
-        double a = 1.4025;
-        double b = 2.908-1.4025;
-        double m = 2300;
-        double J = 3071;
-        double Cf = 107816;
-        double Cr = 173478;
-        double K = 15.88;
-        DynamicBike dynamic_bike = DynamicBike(a,b,m,J,Cf,Cr,K);
-        ros::Subscriber steer_cmd_sub;
-        ros::Subscriber acc_cmd_sub;
-        ros::Publisher odom_pub;
-        ros::Publisher gear_report_pub;
-        ros::Publisher misc_report_pub;
-        tf2_ros::TransformBroadcaster tf2_broadcaster;
-        ros::Rate loop_rate = 50;
-        double ros_time;
-        double sim_time;
-        double steer_cmd;
-        double speed_cmd;
-    public:
-        DynamicBikeNode(ros::NodeHandle *nh) {
-            // Initialize parameters.
-            a = 1.4025;
-            b = 2.908-1.4025;
-            m = 2300;
-            J = 3071;
-            Cf = 107816;
-            Cr = 173478;
-            K = 15.88;
+  private:
+    double a = 1.4025;
+    double b = 2.908-1.4025;
+    double m = 2300;
+    double J = 3071;
+    double Cf = 107816;
+    double Cr = 173478;
+    double K = 15.88;
+    DynamicBike dynamic_bike = DynamicBike(a,b,m,J,Cf,Cr,K);
+    ros::Subscriber steer_cmd_sub;
+    ros::Subscriber acc_cmd_sub;
+    ros::Publisher odom_pub;
+    ros::Publisher gear_report_pub;
+    ros::Publisher misc_report_pub;
+    tf2_ros::TransformBroadcaster tf2_broadcaster;
+    ros::Rate loop_rate = 50.0;
+    double ros_time;
+    double sim_time;
+    double steer_cmd;
+    double speed_cmd;
+  public:
+    DynamicBikeNode(ros::NodeHandle *nh) {
+        // Initialize parameters.
+        a = 1.4025;
+        b = 2.908-1.4025;
+        m = 2300;
+        J = 3071;
+        Cf = 107816;
+        Cr = 173478;
+        K = 15.88;
 
-            // Create subscribers.
-            steer_cmd_sub = nh->subscribe("vehicle/steering_cmd", 10, 
-                &DynamicBikeNode::steerCallback, this);
-            acc_cmd_sub = nh->subscribe("vehicle/accelerator_pedal_cmd", 10, 
-                &DynamicBikeNode::accCallback, this);
+        // Create subscribers.
+        steer_cmd_sub = nh->subscribe("vehicle/steering_cmd", 10, 
+            &DynamicBikeNode::steerCallback, this);
+        acc_cmd_sub = nh->subscribe("vehicle/accelerator_pedal_cmd", 10, 
+            &DynamicBikeNode::accCallback, this);
 
-            // Create publishers.
-            odom_pub = nh->advertise<nav_msgs::Odometry>("novatel/oem7/odom", 1);
-            gear_report_pub = nh->advertise<raptor_dbw_msgs::GearReport>(
-                "/vehicle/gear_report", 10
-            );
-            misc_report_pub = nh->advertise<raptor_dbw_msgs::MiscReport>(
-                "/vehicle/misc_report", 10
-            );
+        // Create publishers.
+        odom_pub = nh->advertise<nav_msgs::Odometry>("novatel/oem7/odom", 1);
+        gear_report_pub = nh->advertise<raptor_dbw_msgs::GearReport>(
+            "/vehicle/gear_report", 10
+        );
+        misc_report_pub = nh->advertise<raptor_dbw_msgs::MiscReport>(
+            "/vehicle/misc_report", 10
+        );
 
-            // Initialize vehicle time.
+        // Initialize vehicle time.
+        ros_time = ros::Time::now().toSec();
+        dynamic_bike.SetTime(ros_time);
+        sim_time = dynamic_bike.GetTime();
+
+        // Initialize vehicle state.
+        dynamic_bike.SetYaw(-45 * 3.14 / 180.0); // 3.14 / 2.0
+    }
+
+    void steerCallback(const raptor_dbw_msgs::SteeringCmd& msg) {
+        steer_cmd = msg.angle_cmd;
+    }
+
+    void accCallback(const raptor_dbw_msgs::AcceleratorPedalCmd& msg) {
+        speed_cmd = msg.speed_cmd;
+    }
+
+    void run() {
+        // Loop until node is killed.
+        while (ros::ok()) {
+            ////////////////////
+            // Get inputs.
+            ////////////////////
+            
+            ros::spinOnce();
+            double inputs[2] = {steer_cmd, speed_cmd};
+            dynamic_bike.SetInputs(inputs);
+
+            ////////////////////
+            // Simulate.
+            ////////////////////
+
             ros_time = ros::Time::now().toSec();
-            dynamic_bike.SetTime(ros_time);
             sim_time = dynamic_bike.GetTime();
-
-            // Initialize vehicle state.
-            dynamic_bike.SetYaw(-45 * 3.14 / 180.0); // 3.14 / 2.0
-        }
-
-        void steerCallback(const raptor_dbw_msgs::SteeringCmd& msg) {
-            steer_cmd = msg.angle_cmd;
-        }
-
-        void accCallback(const raptor_dbw_msgs::AcceleratorPedalCmd& msg) {
-            speed_cmd = msg.speed_cmd;
-        }
-
-        void run() {
-            // Loop until node is killed.
-            while (ros::ok()) {
-                ////////////////////
-                // Get inputs.
-                ////////////////////
-                
-                ros::spinOnce();
-                double inputs[2] = {steer_cmd, speed_cmd};
-                dynamic_bike.SetInputs(inputs);
-
-                ////////////////////
-                // Simulate.
-                ////////////////////
-
-                ros_time = ros::Time::now().toSec();
+            while (sim_time < ros_time) {
+                dynamic_bike.Advance();
                 sim_time = dynamic_bike.GetTime();
-                while (sim_time < ros_time) {
-                    dynamic_bike.Advance();
-                    sim_time = dynamic_bike.GetTime();
-                }
-                
-                ////////////////////
-                // Publish topics.
-                ////////////////////
-
-                // Get vehicle position and velocity.
-                tf2::Quaternion q;
-                q.setRPY(0.0, 0.0, dynamic_bike.GetYaw());
-                q.normalize();
-                double pos[2];
-                dynamic_bike.GetPos(pos);
-                double pos_dt[2];
-                dynamic_bike.GetPosDt(pos_dt);
-
-                // Publish the transform over tf.
-                geometry_msgs::TransformStamped odom_trans;
-                odom_trans.header.stamp = ros::Time(ros_time);
-                odom_trans.header.frame_id = "odom";
-                odom_trans.child_frame_id = "base_link";
-
-                odom_trans.transform.translation.x = pos[0];
-                odom_trans.transform.translation.y = pos[1];
-                odom_trans.transform.rotation = tf2::toMsg(q);
-
-                tf2_broadcaster.sendTransform(odom_trans);
-
-                // Publish odom message.
-                nav_msgs::Odometry odom;
-                odom.header.stamp = ros::Time(ros_time);
-                odom.header.frame_id = "odom";
-                odom.child_frame_id = "base_link";
-
-                odom.twist.twist.linear.x = pos_dt[0];
-                odom.twist.twist.linear.y = pos_dt[1];
-                odom.twist.twist.angular.z = dynamic_bike.GetYawRate();
-                
-                odom.pose.pose.position.x = pos[0];
-                odom.pose.pose.position.y = pos[1];
-                odom.pose.pose.orientation = tf2::toMsg(q);
-
-                odom_pub.publish(odom);
-
-                // Publish misc report.
-                raptor_dbw_msgs::MiscReport misc_report_msg;
-                misc_report_msg.by_wire_ready = true;
-                misc_report_msg.drive_by_wire_enabled = true;
-                misc_report_msg.comms_fault = false;
-                misc_report_msg.general_actuator_fault = false;
-                misc_report_msg.general_driver_activity = false;
-
-                misc_report_pub.publish(misc_report_msg);
-
-                // Publish gear report.
-                raptor_dbw_msgs::GearReport gear_report_msg;
-                gear_report_msg.state.gear = raptor_dbw_msgs::Gear::DRIVE;
-
-                gear_report_pub.publish(gear_report_msg);
-
-                ////////////////////
-                // Wait.
-                ////////////////////
-
-                loop_rate.sleep();
             }
+            
+            ////////////////////
+            // Publish topics.
+            ////////////////////
+
+            // Get vehicle position and velocity.
+            tf2::Quaternion q;
+            q.setRPY(0.0, 0.0, dynamic_bike.GetYaw());
+            q.normalize();
+            double pos[2];
+            dynamic_bike.GetPos(pos);
+            double pos_dt[2];
+            dynamic_bike.GetPosDt(pos_dt);
+
+            // Publish the transform over tf.
+            geometry_msgs::TransformStamped odom_trans;
+            odom_trans.header.stamp = ros::Time(ros_time);
+            odom_trans.header.frame_id = "odom";
+            odom_trans.child_frame_id = "base_link";
+
+            odom_trans.transform.translation.x = pos[0];
+            odom_trans.transform.translation.y = pos[1];
+            odom_trans.transform.rotation = tf2::toMsg(q);
+
+            tf2_broadcaster.sendTransform(odom_trans);
+
+            // Publish odom message.
+            nav_msgs::Odometry odom;
+            odom.header.stamp = ros::Time(ros_time);
+            odom.header.frame_id = "odom";
+            odom.child_frame_id = "base_link";
+
+            odom.twist.twist.linear.x = pos_dt[0];
+            odom.twist.twist.linear.y = pos_dt[1];
+            odom.twist.twist.angular.z = dynamic_bike.GetYawRate();
+            
+            odom.pose.pose.position.x = pos[0];
+            odom.pose.pose.position.y = pos[1];
+            odom.pose.pose.orientation = tf2::toMsg(q);
+
+            odom_pub.publish(odom);
+
+            // Publish misc report.
+            raptor_dbw_msgs::MiscReport misc_report_msg;
+            misc_report_msg.by_wire_ready = true;
+            misc_report_msg.drive_by_wire_enabled = true;
+            misc_report_msg.comms_fault = false;
+            misc_report_msg.general_actuator_fault = false;
+            misc_report_msg.general_driver_activity = false;
+
+            misc_report_pub.publish(misc_report_msg);
+
+            // Publish gear report.
+            raptor_dbw_msgs::GearReport gear_report_msg;
+            gear_report_msg.state.gear = raptor_dbw_msgs::Gear::DRIVE;
+
+            gear_report_pub.publish(gear_report_msg);
+
+            ////////////////////
+            // Wait.
+            ////////////////////
+
+            loop_rate.sleep();
         }
+    }
 };
 
 
